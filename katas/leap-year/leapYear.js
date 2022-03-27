9cb13309ba04f23f0858c73499cf5e9b925c19d6
Myoung <mhbae93@gmail.com>
2022-03-27 15:40:00 +0100

    second refactor using explanitory methods

--- katas/leap-year/leapYear.js
+++ katas/leap-year/leapYear.js
@@ -1,11 +1,33 @@
-const leapYear = (year) => {
-  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+// const leapYear = (year) => {
+//   if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-module.exports = leapYear;
+// module.exports = leapYear;
+
+class LeapYear {
+  constructor(year) {
+    this.year = year;
+  }
+  check() {
+    return (
+      this.isDivisibleBy(4) &&
+      !(this.isDivisibleBy(100) && this.notDivisibleBy(400))
+    );
+  }
+
+  isDivisibleBy(number) {
+    return this.year % number === 0;
+  }
+
+  notDivisibleBy(number) {
+    return this.year % number !== 0;
+  }
+}
+
+module.exports = LeapYear;
--- katas/leap-year/leapYear.test.js
+++ katas/leap-year/leapYear.test.js
@@ -1,19 +1,41 @@
-const leapYear = require("./leapYear");
+const LeapYear = require("./leapYear");
 
-describe("leapyear()", () => {
-  it("should return false if not divisible by 4", () => {
-    expect(leapYear(1997)).toBe(false);
+// describe("leapyear()", () => {
+//   it("should return false if not divisible by 4", () => {
+//     expect(leapYear(1997)).toBe(false);
+//   });
+
+//   it("should return true if divisible by 4", () => {
+//     expect(leapYear(1996)).toBe(true);
+//   });
+
+//   it("should return true if a year is divisible by 400", () => {
+//     expect(leapYear(1600)).toBe(true);
+//   });
+
+//   it("should return false if a year is dividible by 4 & 100 and not 400", () => {
+//     expect(leapYear(1800)).toBe(false);
+//   });
+// });
+
+describe("leapYear", () => {
+  it("should return false if the year is not divisible by 4", () => {
+    const leapYear = new LeapYear(1997);
+    expect(leapYear.check()).toBe(false);
   });
 
-  it("should return true if divisible by 4", () => {
-    expect(leapYear(1996)).toBe(true);
+  it("should return true if the year is divisible by 4", () => {
+    const leapYear = new LeapYear(1996);
+    expect(leapYear.check()).toBe(true);
   });
 
-  it("should return true if a year is divisible by 400", () => {
-    expect(leapYear(1600)).toBe(true);
+  it("should return true if the year is divisible by 400", () => {
+    const leapYear = new LeapYear(1600);
+    expect(leapYear.check()).toBe(true);
   });
 
-  it("should return false if a year is dividible by 4 & 100 and not 400", () => {
-    expect(leapYear(1800)).toBe(false);
+  it("should return false if divisible by 100 but not 400", () => {
+    const leapYear = new LeapYear(1800);
+    expect(leapYear.check()).toBe(false);
   });
 });
