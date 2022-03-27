51ae81f2c714a5af4b3dcca9f6ef4221731125df
Myoung <mhbae93@gmail.com>
2022-03-27 14:50:19 +0100

    all tests passing

--- katas/leap-year/leapYear.js
+++ katas/leap-year/leapYear.js
@@ -1,6 +1,11 @@
 const leapYear = (year) => {
-  if (year % 4 === 0) return true;
-  return false;
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = leapYear;
--- katas/leap-year/leapYear.test.js
+++ katas/leap-year/leapYear.test.js
@@ -8,4 +8,12 @@ describe("leapyear()", () => {
   it("should return true if divisible by 4", () => {
     expect(leapYear(1996)).toBe(true);
   });
+
+  it("should return true if a year is divisible by 400", () => {
+    expect(leapYear(1600)).toBe(true);
+  });
+
+  it("should return false if a year is dividible by 4 & 100 and not 400", () => {
+    expect(leapYear(1800)).toBe(false);
+  });
 });
